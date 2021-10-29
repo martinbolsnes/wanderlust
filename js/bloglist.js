5780118189d9aa7ee28a8560001371d109e082a9
martinbolsnes <martinbolsnes@icloud.com>
2021-10-29 14:59:40 +0200

    changed fetch url

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,4 +1,4 @@
-const postsUrl = 'https://api.martinbols.tech/wp-json/wp/v2/posts?_embed';
+const postsUrl = "https://wanderlust-api-123.herokuapp.com/blogposts";
 
 async function getPosts(postsUrl) {
   try {
@@ -7,15 +7,15 @@ async function getPosts(postsUrl) {
     const postsResults = jsonFromServer;
     console.log(postsResults);
 
-    document.querySelector('.loading').classList.add('hide');
+    document.querySelector(".loading").classList.add("hide");
 
     for (let i = 0; i < postsResults.length; i++) {
-      document.querySelector('.blogList__section').innerHTML += `
+      document.querySelector(".blogList__section").innerHTML += `
       <div class="card__containerBlog">
       <a href="blog.html?id=${postsResults[i].id}">
-        <img class="cardBlog" src="${postsResults[i]._embedded['wp:featuredmedia']['0'].media_details.sizes['1536x1536'].source_url}"/>
+        <img class="cardBlog" src="${postsResults[i].image_url}"/>
       </a>
-        <h3 class="cardTitleBlog">${postsResults[i].title.rendered}</h3>
+        <h3 class="cardTitleBlog">${postsResults[i].title}</h3>
         </div>`;
 
       if (i === 9) {
@@ -23,24 +23,24 @@ async function getPosts(postsUrl) {
       }
     }
   } catch (error) {
-    document.querySelector('.alert').innerHTML = showAlertTouser(
-      'An error occured (Cannot load content)',
-      'error'
+    document.querySelector(".alert").innerHTML = showAlertTouser(
+      "An error occured (Cannot load content)",
+      "error"
     );
   } finally {
     setTimeout(function () {
-      document.querySelector('.alert').innerHTML = '';
+      document.querySelector(".alert").innerHTML = "";
     }, 3000);
   }
 }
 
 getPosts(postsUrl);
 
-const viewMoreBtn = document.getElementById('viewMoreBtn');
-const viewMoreDiv = document.querySelector('.viewMoreDiv');
+const viewMoreBtn = document.getElementById("viewMoreBtn");
+const viewMoreDiv = document.querySelector(".viewMoreDiv");
 
 viewMoreBtn.onclick = function () {
-  getPosts(postsUrl + '&page=2');
+  getPosts(postsUrl + "&page=2");
   viewMoreDiv.innerHTML = ` <div class="loading">
   <img
       src="https://flevix.com/wp-content/uploads/2019/07/Ajax-Preloader.gif"
@@ -49,5 +49,5 @@ viewMoreBtn.onclick = function () {
   setTimeout(function () {
     viewMoreDiv.innerHTML = ``;
   }, 1000);
-  viewMoreBtn.style.display = 'none';
+  viewMoreBtn.style.display = "none";
 };
