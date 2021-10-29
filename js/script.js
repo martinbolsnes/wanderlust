3760303ef9cee5d80907474b9edc07d20b621922
martinbolsnes <martinbolsnes@icloud.com>
2021-10-29 14:47:22 +0200

    changed fetch url

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-const postsUrl = '';
+const postsUrl = "";
 
 async function getPosts() {
   try {
     const repsonse = await fetch(
-      'https://api.martinbols.tech/wp-json/wp/v2/posts/?_embed&per_page=20'
+      "https://wanderlust-api-123.herokuapp.com/blogposts"
     );
     const jsonFromServer = await repsonse.json();
     const postsResults = jsonFromServer;
     console.log(postsResults);
 
-    document.querySelector('.loadingHome').classList.add('hide');
+    //document.querySelector(".loadingHome").classList.add("hide");
 
     for (let i = 0; i < postsResults.length; i++) {
-      document.querySelector('.carousel__content').innerHTML += `
+      document.querySelector(".carousel__content").innerHTML += `
       <div class="card__container">
       <a href="blog.html?id=${postsResults[i].id}">
-        <img class="cardHome" src="${postsResults[i]._embedded['wp:featuredmedia']['0'].media_details.sizes['1536x1536'].source_url}"/>
-        <h3 class="cardTitleHome">${postsResults[i].title.rendered}</h3>        
+        <img class="cardHome" src="${postsResults[i].image_url}"/>
+        <h3 class="cardTitleHome">${postsResults[i].title}</h3>        
         </a>
         </div>`;
 
@@ -25,13 +25,13 @@ async function getPosts() {
       }
     }
   } catch {
-    document.querySelector('.alert').innerHTML = showAlertTouser(
-      'An error occured (Cannot load content)',
-      'error'
+    document.querySelector(".alert").innerHTML = showAlertTouser(
+      "An error occured (Cannot load content)",
+      "error"
     );
   } finally {
     setTimeout(function () {
-      document.querySelector('.alert').innerHTML = '';
+      document.querySelector(".alert").innerHTML = "";
     }, 3000);
   }
 }
@@ -39,30 +39,30 @@ async function getPosts() {
 getPosts(postsUrl);
 
 const gap = 10;
-const carousel = document.querySelector('.carousel');
-const content = document.querySelector('.carousel__content');
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
+const carousel = document.querySelector(".carousel");
+const content = document.querySelector(".carousel__content");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
 
-next.addEventListener('click', (e) => {
+next.addEventListener("click", (e) => {
   carousel.scrollBy(width + gap, 0);
   if (carousel.scrollWidth !== 0) {
-    prev.style.display = 'flex';
+    prev.style.display = "flex";
   }
   if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = 'none';
+    next.style.display = "none";
   }
 });
 
-prev.addEventListener('click', (e) => {
+prev.addEventListener("click", (e) => {
   carousel.scrollBy(-(width + gap), 0);
   if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = 'none';
+    prev.style.display = "none";
   }
   if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = 'flex';
+    next.style.display = "flex";
   }
 });
 
 let width = carousel.offsetWidth;
-window.addEventListener('resize', (e) => (width = carousel.offsetWidth));
+window.addEventListener("resize", (e) => (width = carousel.offsetWidth));
